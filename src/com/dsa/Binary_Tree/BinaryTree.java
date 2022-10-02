42c36c37d429a2cf7fc5f9b1e314c1ce526f60b0
PramodThapa <thapa1pramod82@gmail.com>
2022-10-02 22:15:49 +0545

    Add binary tree traversal.

--- src/com/dsa/Binary_Tree/BinaryTree.java
+++ src/com/dsa/Binary_Tree/BinaryTree.java
@@ -1,8 +1,17 @@
 package src.com.dsa.Binary_Tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTree {
 
     Node root;
+
+    List<Long> inOrderTraversalNode = new ArrayList<>();
+
+    List<Long> preOrderTraversalNode = new ArrayList<>();
+
+    List<Long> postOrderTraversalNode = new ArrayList<>();
 
     /**
      * No args constructor.
@@ -18,6 +27,60 @@ public class BinaryTree {
      */
     BinaryTree(Long nodeValue){
         this.root = new Node(nodeValue);
+    }
+
+    /***
+     * Recursive function to traverse binary tree.
+     *
+     * @param node
+     */
+    public void inOrderTraversal(Node node){
+
+        if(node == null){
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        inOrderTraversalNode.add(node.nodeValue);
+
+        inOrderTraversal(node.right);
+
+    }
+
+    /**
+     * Post order traversal.
+     *
+     * @param node
+     */
+    public void postOrderTraversal(Node node){
+        if(node == null){
+            return;
+        }
+
+        postOrderTraversal(node.left);
+
+        postOrderTraversal(node.right);
+
+        postOrderTraversalNode.add(node.nodeValue);
+    }
+
+    /**
+     * Pre order traversal.
+     *
+     * @param node
+     */
+    public void preOrderTraversal(Node node){
+        if(node == null){
+            return;
+        }
+
+        preOrderTraversalNode.add(node.nodeValue);
+
+        preOrderTraversal(node.left);
+
+        preOrderTraversal(node.right);
+
     }
 
 }
